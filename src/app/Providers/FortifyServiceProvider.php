a9f26f4ff3169e84d5ecd2f340ecd7de7eecaab5
fukuda <sfu983667@gmail.com>
2024-05-25 03:24:38 +0900

    登録→ログイン

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -8,11 +8,13 @@ use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
 use Laravel\Fortify\Contracts\LogoutResponse;
+use Laravel\Fortify\Events\Registered;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -36,6 +38,7 @@ class FortifyServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Fortify::createUsersUsing(CreateNewUser::class);
+
         Fortify::registerView(function () {
             return view('auth.register');
         });
@@ -44,9 +47,17 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.login');
         });
 
+        // ユーザーが登録後に自動ログインを無効化する。したかった。できなかった。
+        // Fortify::authenticateUsing(function (Request $request) {
+        //     return null;
+        // });
+
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
             return Limit::perMinute(10)->by($email . $request->ip());
         });
+
+        // Registeredイベントのリスナーを削除する（自動ログインを無効化するため）
+        // Event::forget(Registered::class);
     }
 }
